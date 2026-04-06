904f0c622c62855a0cb284d5175aa6577eb05157
Cristopher <142592196+PixelForge45@users.noreply.github.com>
2026-04-06 15:18:12 +0000

    conversoes

---
+++ conversoes.js
@@ -0,0 +1,9 @@
+// tipo dados
+//booleanos
+
+// conersoes implicitas
+
+ const numero = 456;
+ const numeroString = "456";
+
+ console.log(numero + Number(numeroString))
--- falso-verdadeiro.js
+++ falso-verdadeiro.js
@@ -1,7 +1,21 @@
-const usuariologado = true;
-const contapaga = false;
+//const usuariologado = true;
+//const contapaga = false;
 
 //0 ==> false
 //1==> verdadeiro
 
-console.log(0 == false);
+//console.log(0 == false);
+//console.log(1 ==true);
+//console.log("" == false);
+
+//null
+//undefined
+
+let minhavar;
+let varNull = null
+
+let numero = 3;
+let texto = "Alura";
+
+console.log(typeof minhavar)
+console.log(typeof varNull)
