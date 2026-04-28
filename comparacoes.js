6db5343fe358be7985ddd2c93fe914ca751bb9cf
Cristopher <142592196+PixelForge45@users.noreply.github.com>
2026-04-28 15:23:56 +0000

    3333

---
+++ comparacoes.js
@@ -0,0 +1,17 @@
+//==(comparação implicita)
+
+const numero = 5
+const texto = "5"
+
+console.log(numero === texto)
+
+//typerof
+console.log(typeof numero)
+console.log(typeof texto)
+
+// == só compara o valor
+//=== compara o valor e tipo de dados
+
+// conversão explicita
+// Number()<-- numero
+// String()<-- texto
---
+++ teampletes-strings.js
@@ -0,0 +1,4 @@
+const nome = Cristopher;
+const apresentacao = "Meu nome é " = nome;
+
+console.log(apresentacao)
