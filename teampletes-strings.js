090aa8c2cc5302687ef50e6ccf7b25c64732192a
Cristopher <142592196+PixelForge45@users.noreply.github.com>
2026-04-29 15:24:48 +0000

    return

--- teampletes-strings.js
+++ teampletes-strings.js
@@ -1,4 +1,7 @@
-const nome = Cristopher;
-const apresentacao = "Meu nome é " = nome;
+const nome = "Cristopher";
+const idade = 2026-2009
+const cidadeDeNascimento = "Curitiba"
+
+//const apresentacao = "Meu nome é " + nome + ", minha idade é " + idade + " Nasci em " + cidadeDeNascimento;
 
 console.log(apresentacao)
