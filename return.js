26ac361729f09fdd8e453afebf40e7c5ed313fb7
Cristopher <142592196+PixelForge45@users.noreply.github.com>
2026-05-04 15:25:18 +0000

    eeeeee

--- return.js
+++ return.js
@@ -1,12 +1,21 @@
-function soma(numero1, numero2, numero3) {
- return numero1 + numero2 + numero3;
+//function soma(numero1, numero2, numero3) {
+ //return numero1 + numero2 + numero3;
+//}
+
+//console.log(soma(2, 2, 96))
+//console.log(soma(3, 3, 89))
+//console.log(soma(66, 222, 54))
+
+//function nomeIdade(nome, idade) {
+  //  return `Meu nome é ${nome} e minha idade é ${idade}`;
+    ///}
+    //console.log(nomeIdade("Cristopher", 17))
+
+function multiplicacao(numero1, numero2) {
+    return numero1 * numero2;
+}
+function soma(numero1, numero2) {
+    return numero1 + numero2;
 }
 
-console.log(soma(2, 2, 96))
-console.log(soma(3, 3, 89))
-console.log(soma(66, 222, 54))
-
-function nomeIdade(nome, idade) {
-    return `Meu nome é ${nome} e minha idade é ${idade}`;
-    }
-    console.log(nomeIdade("Cristopher", 17))
+console.log(multiplicacao(soma(5, 5), soma(5,5)))
