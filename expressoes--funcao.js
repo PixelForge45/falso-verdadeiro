7b19ae86bf2528f2d39999074f23ae2e9e378028
Cristopher <142592196+PixelForge45@users.noreply.github.com>
2026-05-05 15:25:05 +0000

    funçoes

---
+++ expressoes--funcao.js
@@ -0,0 +1,11 @@
+//function minhaFuncao(
+    // block os code
+//) {
+
+    // myFunction('paran') {
+
+    const soma = function(numero1, numero2) {
+        return numero1 + numero2;
+    }
+    console.log(soma(5, 5))
+}
